3d03330fc0b4a14f129b2be3c18a014a9436af83
AssassinGhostFIT <assassinghost0@gmail.com>
2025-08-27 21:14:43 -0500

    Agregado Perfil (HTML, CSS, JS) + estructura Android simulada

---
+++ assets/js/perfil.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsBtn = document.querySelector(".settings-btn");
+
+  settingsBtn.addEventListener("click", () => {
+    alert("Aquí irán las configuraciones, política de privacidad, etc.");
+  });
+});
